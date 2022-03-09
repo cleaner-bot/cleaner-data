54186f5e87fcf82f9a14cf3f57172dff9a2ad55a
Leo Developer <git@leodev.xyz>
2022-03-09 15:39:19 +0100

    add dependabot and requires

--- setup.py
+++ setup.py
@@ -3,6 +3,13 @@ from setuptools import setup, find_namespace_packages  # type: ignore
 from cleaner_data import __version__
 
 
+def parse_requirements_file(path):
+    with open(path) as file:
+        lines = file.read().splitlines()
+    dependencies = (x.strip() for x in lines)
+    return [x for x in dependencies if x and not x.startswith("#")]
+
+
 setup(
     name="cleaner_data",
     version=__version__,
@@ -10,5 +17,6 @@ setup(
     author="Leo Developer",
     author_email="git@leodev.xyz",
     description="data of the cleaner",
+    requires=parse_requirements_file("requirements.txt"),
     packages=find_namespace_packages(include=["cleaner_data*"]),
 )
